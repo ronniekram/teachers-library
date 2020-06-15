a5850e4f8fc3a4613788769fcc51a2e9a67d9890
ronniekram <arynn.boniface@gmail.com>
2020-06-15 12:01:42 -0700

    added logout/delete account options - not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :lesson_plans
   resources :books
-  resources :users, except: [:index, :new, :create] do 
+  resources :users, except: [:index, :new, :create, :destroy] do 
     resources :books, only: [:show, :index]
     resources :lesson_plans, only: [:show, :index]
   end 
@@ -9,9 +9,11 @@ Rails.application.routes.draw do
   root 'users#index'
   get '/signup', to: 'users#new', as: 'signup'
   post '/signup', to: 'users#create'
+  delete '/delete', to: 'users#destroy'
+
   get '/login', to: 'sessions#new', as: 'login' 
   post '/sessions', to: 'sessions#create', as: 'sessions'
-  delete '/sessions/', to: 'sessions#destroy'
+  delete '/sessions', to: 'sessions#destroy', as: 'logout'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
