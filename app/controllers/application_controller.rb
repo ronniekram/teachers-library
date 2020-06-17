959eeb3fafc9ccdd0d799c2f78a425f33900d626
ronniekram <arynn.boniface@gmail.com>
2020-06-17 14:59:17 -0700

    fixed added devise fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception, prepend: true
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || root_path
+  end
+
+  protected 
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:user_name, :email, :password, :school, :grade)}
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:user_name, :email, :password, :current_password, :school, :grade)}
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
-        #  , :omniauth_providers => [:github]
-
-  # def self.new_with_session(params, session)
-  #   super.tap do |user|
-  #     if data = session['devise.github_data'] && session['devise.github_data']['extra']['raw_info']
-  #     user.email = data['email'] if user.email.blank?
-  #     end
-  #   end
-  # end
 
   def self.from_omniauth(auth)  
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
