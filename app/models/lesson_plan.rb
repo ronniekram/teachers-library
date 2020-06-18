78679691a403a8798edd71b8f6b04dde401d200d
ronniekram <arynn.boniface@gmail.com>
2020-06-17 21:13:27 -0700

    adjusted model relationships

--- app/models/lesson_plan.rb
+++ app/models/lesson_plan.rb
@@ -1,7 +1,7 @@
 class LessonPlan < ApplicationRecord
   belongs_to :user
   has_many :subjects
-  has_many :books, { => extending: Subject }through: :subjects
+  has_many :books, through: :subjects
 
   validates :duration, :numericality => true, :length => { :within => 1..30 }
   validates :name, :uniqueness => true
