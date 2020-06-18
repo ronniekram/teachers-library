8327d255d0b3073136b22d5516bed660c5d7a88a
ronniekram <arynn.boniface@gmail.com>
2020-06-18 13:14:44 -0700

    added error message partials to books and lesson plans

--- app/models/lesson_plan.rb
+++ app/models/lesson_plan.rb
@@ -1,6 +1,6 @@
 class LessonPlan < ApplicationRecord
   belongs_to :user
-  has_many :subjects
+  has_many :subjects, :dependent => :destroy
   has_many :books, through: :subjects
 
   validates :duration, :numericality => true, :length => { :within => 1..30 }
