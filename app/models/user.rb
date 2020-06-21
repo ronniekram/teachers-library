a3c6d300b9b3f91a9603d8c34198f38904efb0e1
ronniekram <arynn.boniface@gmail.com>
2020-06-21 13:41:17 -0700

    worked on styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,6 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      # user.name = auth.info.name   # assuming the user model has a name
-      # user.image = auth.info.image # assuming the user model has an image
-      user.skip_confirmation!
     end
   end
 
