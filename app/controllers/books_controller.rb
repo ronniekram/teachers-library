2e9b9e95bbb20b52d6bba15e04b80f9930f458f6
ronniekram <arynn.boniface@gmail.com>
2020-06-22 12:43:49 -0700

    removed actions from books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,4 @@
   class BooksController < ApplicationController
-    before_action :set_book, except: [:index, :new, :create]
 
     def index 
       @books = current_user.books.alphabet
@@ -7,36 +6,19 @@
 
     def new 
       @book = Book.new
-       3.times { @book.subjects.build }
     end 
 
     def create 
       @book = current_user.books.build(book_params)
       if @book.save
-        redirect
+        redirect_to book_path(@book)
       else 
         render :new 
       end 
     end 
 
     def show
-    end 
-
-    def edit 
-    end 
-
-    def update
-      @book.update(book_params)
-      if @book.save 
-        redirect
-      else 
-        render :edit 
-      end 
-    end 
-
-    def destroy
-      @book.destroy
-      redirect_to user_books_path(current_user)
+      @book = Book.find(params[:id])
     end 
 
     private
@@ -44,19 +26,7 @@
     def book_params
       params.require(:book).permit(:title, 
       :author,
-      :subject, 
-      :user_id, 
-      :subject_ids => [],
-      subjects_attributes: [:id, :name, :_destroy]
+      :subject
         )
-    end 
-
-    def set_book
-      @book = Book.find(params[:id])
-    end 
-
-    def redirect 
-      redirect_to user_book_path(current_user, @book)
-    end 
-    
+    end     
   end
