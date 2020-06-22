89284b41579aebcd06db116a8fcda2112b8848ac
ronniekram <arynn.boniface@gmail.com>
2020-06-21 21:39:30 -0700

    fixed sign up error

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,10 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
-      user.name = auth.info.name
-      user.uid = auth.uid
       user.email = auth.info.email
+      user.uid = auth.uid
       user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
     end
   end
 
