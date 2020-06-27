fcb07c5b67f31ea0b24d09672b55299a74c21af7
ronniekram <arynn.boniface@gmail.com>
2020-06-27 15:56:14 -0700

    removed create book in lesson plan form

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,9 @@
 class Book < ApplicationRecord
   has_many :lesson_plans
   has_many :subjects, through: :lesson_plans
-  validates :title, :author, :presence => true
-  validates :title, uniqueness: true;
-  validates :pages, numericality: { greater_than_or_equal_to: 1 }
+  validates :title, :author, :publisher, presence: true
+  validates :title, uniqueness: true
+  validates :pages, presence: true, numericality: { greater_than_or_equal_to: 1 }
 
   scope :alphabet, -> { order(title: :asc) }
 end
--- app/models/lesson_plan.rb
+++ app/models/lesson_plan.rb
@@ -6,22 +6,13 @@ class LessonPlan < ApplicationRecord
 
   validates :duration, presence: true, inclusion: { in: (1..30), message: "cannot be less than 1 day or more than 30 days." }
   validates :name, :start_date, presence: true, uniqueness: true
-  # validates :start_date, presence: true, uniqueness: true, in_past: true
   validates :start_date,
   date: { after: Proc.new { Date.today }, message: 'must be after today' },
   on: :create
+
   scope :dates, -> { order(start_date: :asc) }
   
   def lesson_start
     start_date.strftime("%B %d, %Y")
   end 
-
-  def books_attributes=(book_attributes)
-    book_attributes.values.each do |book_attribute|
-      if book_attribute["title"].present? && book_attribute["author"].present?
-        book = Book.find_or_create_by(book_attribute)
-        self.books << book
-      end 
-    end
-  end
 end
