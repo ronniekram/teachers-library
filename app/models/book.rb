d94588a82a69319dc5debbb2565b0af50a607c0c
ronniekram <arynn.boniface@gmail.com>
2020-06-27 10:34:47 -0700

    removed school option from form

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
   has_many :lesson_plans
   has_many :subjects, through: :lesson_plans
   validates :title, :author, :presence => true
+  validates :title, uniqueness: true;
 
   scope :alphabet, -> { order(title: :asc) }
 
