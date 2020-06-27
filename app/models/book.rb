40f8bcd514354b04a207c006ea1f30ac240f0e11
ronniekram <arynn.boniface@gmail.com>
2020-06-27 14:09:46 -0700

    updated pages validator

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ApplicationRecord
   has_many :subjects, through: :lesson_plans
   validates :title, :author, :presence => true
   validates :title, uniqueness: true;
+  validates :pages, numericality: { greater_than_or_equal_to: 1 }
 
   scope :alphabet, -> { order(title: :asc) }
 end
--- app/models/lesson_plan.rb
+++ app/models/lesson_plan.rb
@@ -2,12 +2,19 @@ class LessonPlan < ApplicationRecord
   belongs_to :user
   belongs_to :subject
   has_many :books
+  accepts_nested_attributes_for :subject, reject_if: :all_blank
+
   validates :duration, presence: true, inclusion: { in: (1..30), message: "cannot be less than 1 day or more than 30 days." }
   validates :name, presence: true, uniqueness: true
-  validates :start_date, presence: true, uniqueness: true
-  accepts_nested_attributes_for :subject, reject_if: :all_blank
+  validates :start_date, presence: true, uniqueness: true, in_past: true
   scope :dates, -> { order(start_date: :asc) }
 
+  def in_past 
+    if start_date.present? && start_date < Date.today
+      errors.add(:start_date, "can't be in the past.")
+    end
+  end 
+  
   def lesson_start
     start_date.strftime("%B %d, %Y")
   end 
