179039558ce79c396ddaa1e42adaa3b69896cafb
ronniekram <arynn.boniface@gmail.com>
2020-06-29 19:45:35 -0700

    added correct div to signup/signin/edit profile forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do 
   devise_for :users, :controllers => {:registrations => 'registrations', :omniauth_callbacks => 'callbacks'}
+
+  root 'home#index'
   
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'devise/registrations#new'  
   end
-
-  root 'home#index'
-  # resources :users, only: [:show] do 
-  #   resources :lesson_plans
-  # end 
 
   scope '/users' do 
     resources :lesson_plans
@@ -17,5 +14,4 @@ Rails.application.routes.draw do
   
   resources :books, except: [:destroy]
   resources :subjects, only: [:new, :create]
-
 end
