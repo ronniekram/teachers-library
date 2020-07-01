2dac945f4e6d65d1bc9d817189c09a290972522d
ronniekram <arynn.boniface@gmail.com>
2020-07-01 11:04:42 -0700

    last minute edits and fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :lesson_plans
-  validates :name, format: { with: /[a-zA-Z]/, message: "Your name cannot contain numbers or symbols." }
+  validates :name, presence: true, format: { with: /[a-zA-Z]/, message: "Your name cannot contain numbers or symbols." }
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
