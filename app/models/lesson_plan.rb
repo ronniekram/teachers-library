1a381289743c95db3082b223d981e00ba3b453e0
ronniekram <arynn.boniface@gmail.com>
2020-06-30 17:52:55 -0700

    tidied up code

--- app/models/lesson_plan.rb
+++ app/models/lesson_plan.rb
@@ -2,13 +2,12 @@ class LessonPlan < ApplicationRecord
   belongs_to :user
   belongs_to :subject
   has_many :books
-  accepts_nested_attributes_for :subject, reject_if: :all_blank
-
   validates :duration, presence: true, inclusion: { in: (1..30), message: "cannot be less than 1 day or more than 30 days." }
   validates :name, :start_date, presence: true, uniqueness: true
   validates :start_date,
   date: { after: Proc.new { Date.today }, message: 'must be after today' },
   on: :create
+  accepts_nested_attributes_for :subject, reject_if: :all_blank
 
   scope :dates, -> { order(start_date: :asc) }
   
